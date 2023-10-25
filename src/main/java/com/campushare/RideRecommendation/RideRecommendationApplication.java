60c6ba5e827dcd5ce95f4c7bf541bc6ad46b404b
ruyicS2023 <123344125+ruyicS2023@users.noreply.github.com>
2023-10-25 16:41:11 -0700

    Add Spring Initializr base project

---
+++ src/main/java/com/campushare/RideRecommendation/RideRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.campushare.RideRecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campushare/RideRecommendation/RideRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campushare.RideRecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
