ef000cd1344f1dba5ac4ba485271a51eef1a856a
ruyicS2023 <123344125+ruyicS2023@users.noreply.github.com>
2023-11-22 23:17:01 -0800

    ga added

--- src/main/java/com/campushare/RideRecommendation/recommendationAlgo/RecommendationAlgorithm.java
+++ src/main/java/com/campushare/RideRecommendation/recommendationAlgo/RecommendationAlgorithm.java
@@ -5,26 +5,128 @@ import com.campushare.RideRecommendation.model.Schedule;
 import com.campushare.RideRecommendation.model.User;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class RecommendationAlgorithm  {
 
+    private final int populationSize = 100;
+    private final double mutationRate = 0.05;
+    private final int maxGenerations = 100;
+    private Random random = new Random();
+
     public List<String> generateRecommendations(String currentUserId, Schedule currentUserSchedule, String currentUserZipcode, List<User> allUsers) {
-        List<String> recommendedUserIds = new ArrayList<>();
-        for (User user : allUsers) {
-            if (!user.getId().equals(currentUserId) &&
-                    user.getZipcode().equals(currentUserZipcode) ||
-                    schedulesMatch(user.getSchedule(), currentUserSchedule)) {
-                recommendedUserIds.add(user.getId());
+        List<List<User>> population = initializePopulation(allUsers, populationSize);
+
+        for (int generation = 0; generation < maxGenerations; generation++) {
+            List<List<User>> newPopulation = new ArrayList<>();
+
+            for (int i = 0; i < populationSize; i++) {
+                List<User> parent1 = selectParent(population);
+                List<User> parent2 = selectParent(population);
+                List<User> offspring = crossover(parent1, parent2);
+                mutate(offspring, allUsers);
+                newPopulation.add(offspring);
+            }
+
+            population = newPopulation;
+        }
+
+        List<User> bestSolution = selectBest(population, currentUserId, currentUserSchedule, currentUserZipcode);
+        return convertToUserIds(bestSolution);
+    }
+
+    private List<List<User>> initializePopulation(List<User> allUsers, int populationSize) {
+        List<List<User>> population = new ArrayList<>();
+        for (int i = 0; i < populationSize; i++) {
+            Collections.shuffle(allUsers);
+            population.add(new ArrayList<>(allUsers.subList(0, Math.min(10, allUsers.size()))));
+        }
+        return population;
+    }
+
+    private List<User> selectParent(List<List<User>> population) {
+        double totalFitness = population.stream().mapToDouble(this::calculateFitness).sum();
+        double slice = random.nextDouble() * totalFitness;
+        double total = 0;
+        for (List<User> individual : population) {
+            total += calculateFitness(individual);
+            if (total >= slice) {
+                return individual;
             }
         }
-        return recommendedUserIds;
+        return population.get(random.nextInt(population.size()));
+    }
+
+    private List<User> crossover(List<User> parent1, List<User> parent2) {
+        List<User> offspring = new ArrayList<>();
+        for (int i = 0; i < parent1.size(); i++) {
+            offspring.add(random.nextBoolean() ? parent1.get(i) : parent2.get(i));
+        }
+        return offspring;
+    }
+
+    private void mutate(List<User> individual, List<User> allUsers) {
+        for (int i = 0; i < individual.size(); i++) {
+            if (random.nextDouble() < mutationRate) {
+                individual.set(i, allUsers.get(random.nextInt(allUsers.size())));
+            }
+        }
+    }
+
+    private List<User> selectBest(List<List<User>> population, String currentUserId, Schedule currentUserSchedule, String currentUserZipcode) {
+        return Collections.max(population, Comparator.comparingDouble(individual -> calculateFitness(individual)));
+    }
+
+    private double calculateFitness(List<User> recommendation, String currentUserId, Schedule currentUserSchedule, String currentUserZipcode) {
+        double score = 0.0;
+        double zipcodeWeight = 1;
+        double scheduleWeight = 3;
+
+        for (User user : recommendation) {
+            if (user.getId().equals(currentUserId)) {
+                continue;
+            }
+
+            if (user.getZipcode().equals(currentUserZipcode)) {
+                score += zipcodeWeight;
+            }
+
+            if (schedulesMatch(user.getSchedule(), currentUserSchedule)) {
+                score += scheduleWeight;
+            }
+        }
+
+        return score;
+    }
+
+    private List<String> convertToUserIds(List<User> users) {
+        List<String> userIds = new ArrayList<>();
+        for (User user : users) {
+            userIds.add(user.getId());
+        }
+        return userIds;
     }
 
     private boolean schedulesMatch(Schedule schedule1, Schedule schedule2) {
-        // Simple logic for matching schedules
-        return schedule1.equals(schedule2);
+        if (schedule1 == null || schedule2 == null) {
+            return false;
+        }
+
+        return schedule1.getEntryTime().equals(schedule2.getEntryTime()) ||
+                schedule1.getExitTime().equals(schedule2.getExitTime());
     }
+
+//    public List<String> generateRecommendations(String currentUserId, Schedule currentUserSchedule, String currentUserZipcode, List<User> allUsers) {
+//        List<String> recommendedUserIds = new ArrayList<>();
+//        for (User user : allUsers) {
+//            if (!user.getId().equals(currentUserId) &&
+//                    user.getZipcode().equals(currentUserZipcode) ||
+//                    schedulesMatch(user.getSchedule(), currentUserSchedule)) {
+//                recommendedUserIds.add(user.getId());
+//            }
+//        }
+//        return recommendedUserIds;
+//    }
+
 }
