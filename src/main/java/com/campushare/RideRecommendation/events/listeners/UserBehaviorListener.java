ef05ef96b0389a1b0b0ab18de6b4cb68ec50eac3
ruyicS2023 <123344125+ruyicS2023@users.noreply.github.com>
2023-11-22 16:34:29 -0800

    observer pattern implemented

--- src/main/java/com/campushare/RideRecommendation/events/listeners/UserBehaviorListener.java
+++ src/main/java/com/campushare/RideRecommendation/events/listeners/UserBehaviorListener.java
@@ -1,12 +1,77 @@
 package com.campushare.RideRecommendation.events.listeners;
 
 import com.campushare.RideRecommendation.events.data.EventData;
+import com.campushare.RideRecommendation.model.Recommendation;
+import com.campushare.RideRecommendation.model.User;
+import com.campushare.RideRecommendation.repositories.UserRepository;
+import com.campushare.RideRecommendation.repositories.RecommendationRepository;
+import com.campushare.RideRecommendation.recommendationAlgo.RecommendationAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
 public class UserBehaviorListener implements EventListener {
+
+    private final UserRepository userRepository;
+    private final RecommendationRepository recommendationRepository;
+    private final RecommendationAlgorithm recommendationAlgorithm;
+
+    @Autowired
+    public UserBehaviorListener(UserRepository userRepository,
+                                RecommendationRepository recommendationRepository,
+                                RecommendationAlgorithm recommendationAlgorithm) {
+        this.userRepository = userRepository;
+        this.recommendationRepository = recommendationRepository;
+        this.recommendationAlgorithm = recommendationAlgorithm;
+    }
+
     @Override
     public void update(EventData eventData) {
-        // Handle the user behavior event
+        if (isUserRelatedEvent(eventData)) {
+            processUserEvent(eventData);
+        }
+    }
+
+    private boolean isUserRelatedEvent(EventData eventData) {
+        return eventData.getSchedule() != null && eventData.getZipcode() != null;
+    }
+
+    private void processUserEvent(EventData eventData) {
+        // Convert EventData to User model
+        User user = new User();
+        user.setId(eventData.getUserId());
+        user.setZipcode(eventData.getZipcode());
+        user.setSchedule(eventData.getSchedule());
+
+        // Save or update user in the database
+        userRepository.save(user);
+
+        // Generate recommendations
+        List<User> allUsers = userRepository.findAll();
+        List<String> recommendations = recommendationAlgorithm.generateRecommendations(
+                eventData.getUserId(),
+                eventData.getSchedule(),
+                eventData.getZipcode(),
+                allUsers
+        );
+
+        // Save the recommendations
+        saveRecommendations(eventData.getUserId(), recommendations);
+    }
+
+    private void saveRecommendations(String userId, List<String> recommendedUserIds) {
+        Recommendation existingRecommendation = recommendationRepository.findByUserId(userId);
+
+        if (existingRecommendation != null) {
+            existingRecommendation.setUsersIds(recommendedUserIds);
+            recommendationRepository.save(existingRecommendation);
+        } else {
+            Recommendation newRecommendation = new Recommendation();
+            newRecommendation.setUserId(userId);
+            newRecommendation.setUsersIds(recommendedUserIds);
+            recommendationRepository.save(newRecommendation);
+        }
     }
 }
-
-
--- src/main/java/com/campushare/RideRecommendation/services/RecommendationService.java
+++ src/main/java/com/campushare/RideRecommendation/services/RecommendationService.java
@@ -1,102 +1,39 @@
 package com.campushare.RideRecommendation.services;
 
 import com.campushare.RideRecommendation.model.Recommendation;
-import com.campushare.RideRecommendation.model.User;
 import com.campushare.RideRecommendation.repositories.RecommendationRepository;
-import com.campushare.RideRecommendation.events.data.EventData;
-import com.campushare.RideRecommendation.events.EventManager;
-import com.campushare.RideRecommendation.repositories.UserRepository;
-import com.campushare.RideRecommendation.recommendationAlgo.RecommendationAlgorithm;
-import com.campushare.RideRecommendation.utils.EventType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class RecommendationService {
 
     private final RecommendationRepository recommendationRepository;
-    private final UserRepository userRepository;
-    private final RecommendationAlgorithm recommendationAlgorithm;
-    private final EventManager eventManager;
 
     @Autowired
-    public RecommendationService(RecommendationRepository recommendationRepository, UserRepository userRepository, UserRepository userRepository1, RecommendationAlgorithm recommendationAlgorithm, EventManager eventManager) {
+    public RecommendationService(RecommendationRepository recommendationRepository) {
         this.recommendationRepository = recommendationRepository;
-        this.userRepository = userRepository;
-        this.recommendationAlgorithm = recommendationAlgorithm;
-        this.eventManager = eventManager;
-
-        // Subscribe to events using the EventManager
-        eventManager.subscribe(EventType.USER_CREATED, this::handleUserEvent);
-        eventManager.subscribe(EventType.USER_UPDATED, this::handleUserEvent);
     }
 
-
-    // Example event handler methods
-    private void handleUserEvent(EventData eventData) {
-        // Check if the event data is relevant for user-related events
-        if (isUserRelatedEvent(eventData)) {
-            // Convert EventData to User model
-            User user = new User();
-            user.setId(eventData.getUserId());
-            user.setZipcode(eventData.getZipcode());
-            user.setSchedule(eventData.getSchedule());
-
-            // Save or update user in the database
-            userRepository.save(user);
-
-            // Generate recommendations
-            List<User> allUsers = userRepository.findAll();
-            List<String> recommendations = recommendationAlgorithm.generateRecommendations(
-                    eventData.getUserId(),
-                    eventData.getSchedule(),
-                    eventData.getZipcode(),
-                    allUsers
-            );
-            saveRecommendations(eventData.getUserId(), recommendations);
-        }
+    public Recommendation createRecommendation(Recommendation recommendation) {
+        // Create a new recommendation
+        return recommendationRepository.save(recommendation);
     }
 
-    private void saveRecommendations(String userId, List<String> recommendedUserIds) {
-        // Check if there's an existing recommendation for this user
-        Recommendation existingRecommendation = recommendationRepository.findByUserId(userId);
-
-        if (existingRecommendation != null) {
-            // If an existing recommendation is found, update it
-            existingRecommendation.setUsersIds(recommendedUserIds);
-            recommendationRepository.save(existingRecommendation);
-        } else {
-            // If there is no existing recommendation, create a new one
-            Recommendation newRecommendation = new Recommendation();
-            newRecommendation.setUserId(userId);
-            newRecommendation.setUsersIds(recommendedUserIds);
-            recommendationRepository.save(newRecommendation);
-        }
+    public Recommendation getRecommendation(String userId) {
+        // Retrieve a recommendation by user ID
+        return recommendationRepository.findByUserId(userId);
     }
 
-
-
-    private boolean isUserRelatedEvent(EventData eventData) {
-        // Implement logic to determine if the event data is for a user-related event
-        // This might involve checking certain fields in EventData
-        return eventData.getSchedule() != null && eventData.getZipcode() != null;
+    public Recommendation updateRecommendation(Recommendation recommendation) {
+        // Update an existing recommendation
+        return recommendationRepository.save(recommendation);
     }
 
-//    public Recommendation createRecommendation(Recommendation recommendation) {
-//        // Save the recommendation to the database
-//        return recommendationRepository.save(recommendation);
-//    }
-//
-//    public Recommendation getRecommendation(String userId) {
-//        // Retrieve the recommendation from the database
-//        return recommendationRepository.findByUserId(userId);
-//    }
-//
-//    public Recommendation generateRecommendations(String userId) {
-//        // Generate recommendations for the user with the given ID
-//        // This could involve running the genetic algorithm to generate new recommendations
-//        return null;
-//    }
+    public void deleteRecommendation(String userId) {
+        // Delete a recommendation by user ID
+        recommendationRepository.deleteById(userId);
+    }
+
+    // Additional methods as needed for handling direct interactions with recommendations
 }
